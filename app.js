bcdee1152ecac9a1af70fa830497b7e80c060601
rahulmhjn <rahul.mahajan676@gmail.com>
2020-07-23 21:46:24 +0530

    setting up hbs layouts

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv");
 require("colors");
@@ -18,6 +19,13 @@ if (process.env.NODE_ENV === "development") {
 
 // Handlebars
 app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", ".hbs");
+
+// Static Folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
+app.use("/", require("./routes/index"));
 
 const PORT = process.env.PORT || 5000;
 
