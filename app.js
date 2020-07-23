2bebf9434836a06e920a089c3520e77318220422
rahulmhjn <rahul.mahajan676@gmail.com>
2020-07-24 01:49:01 +0530

    Google OAuth2 complete

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const path = require("path");
 const express = require("express");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 require("colors");
 const morgan = require("morgan");
 const exphbs = require("express-handlebars");
+const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 const connectDB = require("./config/db");
 
 // Load config
 dotenv.config({ path: "./config/config.env" });
+
+// Passport config
+require("./config/passport")(passport);
 
 connectDB();
 
@@ -21,11 +28,26 @@ if (process.env.NODE_ENV === "development") {
 app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", ".hbs");
 
+// Sessions
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Static Folder
 app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
 app.use("/", require("./routes/index"));
+app.use("/auth", require("./routes/auth"));
 
 const PORT = process.env.PORT || 5000;
 
