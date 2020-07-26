9c68257494c2802bb321f71040058e310d0eac70
rahulmhjn <rahul.mahajan676@gmail.com>
2020-07-26 05:30:11 +0530

    secure delete and user stories

--- routes/stories.js
+++ routes/stories.js
@@ -40,6 +40,27 @@ router.get("/", ensureAuth, async (req, res) => {
   }
 });
 
+// @desc    Show single story
+// @route   GET /stories/:id
+router.get("/:id", ensureAuth, async (req, res) => {
+  try {
+    let story = await Story.findById({ _id: req.params.id })
+      .populate("user")
+      .lean();
+
+    if (!story) {
+      return res.render("error/404");
+    }
+
+    res.render("stories/show", {
+      story,
+    });
+  } catch (error) {
+    console.log(error);
+    res.render("error/500");
+  }
+});
+
 // @desc    Show edit Page
 // @route   GET /stories/edit/:id
 router.get("/edit/:id", ensureAuth, async (req, res) => {
@@ -94,10 +115,40 @@ router.put("/:id", ensureAuth, async (req, res) => {
 // @route   Delete /stories/:id
 router.delete("/:id", ensureAuth, async (req, res) => {
   try {
-    await Story.remove({ _id: req.params.id });
-    res.redirect("/dashboard");
+    let story = await Story.findById({ _id: req.params.id }).lean();
+
+    if (!story) {
+      return res.render("error/404");
+    }
+
+    if (story.user != req.user.id) {
+      res.redirect("/stories");
+    } else {
+      await Story.remove({ _id: req.params.id });
+      res.redirect("/dashboard");
+    }
   } catch (error) {
     console.log(error);
+    res.render("error/500");
+  }
+});
+
+// @desc    User Stories
+// @route   GET /stories/user/:userId
+router.get("/user/:userId", ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({
+      user: req.params.userId,
+      status: "public",
+    })
+      .populate("user")
+      .lean();
+
+    res.render("stories/index", {
+      stories,
+    });
+  } catch (error) {
+    console.log("error");
     res.render("error/500");
   }
 });
